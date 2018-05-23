a9158fb8aded7d4149af08f7473a2096edfbb207
bkrigel <benjamin.krigel@gmail.com>
2018-05-23 18:26:36 -0400

    didn't realize that 20 was a percentage

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :cart, :last_item_price
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @cart = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price*quantity
+    quantity.times {@cart << title}
+    @last_item_price = price
+  end
+
+  def apply_discount
+    if @discount > 0
+      @total -= @total*(@discount.to_f/100)
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @cart
+  end
+
+  def void_last_transaction
+    @cart.pop
+    @total -= @last_item_price
+  end
+
+end
